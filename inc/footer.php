71537b848cc7aabef01ab73e27fb018fcfafe2ea
Jonsku <jonathan.cremieux@gmail.com>
2012-08-23 16:22:12 +0300

    Smaller partner logo in the footer on 2 lines instead of 2 columns.

--- inc/footer.php
+++ inc/footer.php
@@ -1,46 +1,46 @@
-        <!-- footer starts -->
-        <div class="container footer">
-            <div class="row">
-                <div class="span12">
-                    <img class="line" src="<?php echo $config['paths']['base_url']; ?>/img/long_line.png" alt="footer starts">
-                </div>
-            </div>
-            <div class="row">
-                <div class="span12">
-                    <div class="row">
-                        <div class="span4">
-                            <div class="row">
-                                <ul class="span4 footer-nav">
-                                <?php
+<!-- footer starts -->
+
+<div class="container footer">
+  <div class="row">
+    <div class="span12"> <img class="line" src="<?php echo $config['paths']['base_url']; ?>/img/long_line.png" alt="footer starts"> </div>
+  </div>
+  <div class="row">
+    <div class="span12">
+      <div class="row">
+        <div class="span4">
+          <div class="row">
+            <ul class="span4 footer-nav">
+              <?php
                                 foreach($navigation as $label=>$fileName){
                                 ?>
-                                  <li<?php if($page == $label) echo ' class="active"'; ?>><a href="<?php echo $config['paths']['base_url']."/".$_SESSION['locale']."/".$fileName; ?>"><?php s('fi_FI'); echo $label; e(); ?></a></li>
-                                <?php
+              <li<?php if($page == $label) echo ' class="active"'; ?>><a href="<?php echo $config['paths']['base_url']."/".$_SESSION['locale']."/".$fileName; ?>">
+                <?php s('fi_FI'); echo $label; e(); ?>
+                </a></li>
+              <?php
                                 }
                                 ?>
-                                  <li><a href="http://www.facebook.com/siivouspaiva">Facebook</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                            
-                        <ul class="partner span4">
-                            <li><a href="http://www.fida.info/" target="_blank"><img src="<?php echo $config['paths']['base_url']; ?>/partners/fida.png" alt="Fida International"></a></li>
-                            <li><a href="http://helsinkilocomotive.org/" target="_blank"><img src="<?php echo $config['paths']['base_url']; ?>/partners/hnml.png" alt="Helsinki New Media Locomotive"></a></li>
-                            <li><a href="http://www.kierratyskeskus.fi/" target="_blank"><img src="<?php echo $config['paths']['base_url']; ?>/partners/kk.png" alt="Kierrätyskeskus"></a></li>
-                            <li><a href="http://www.lassila-tikanoja.fi" target="_blank"><img src="<?php echo $config['paths']['base_url']; ?>/partners/lt.jpg" alt="Lassila tikanoja"></a></li>
-                        </ul>
-                        <ul class="partner span4">
-                            <li><a href="http://www.technoworld.fi/" target="_blank"><img src="<?php echo $config['paths']['base_url']; ?>/partners/stena.png" alt="STENA Innovative Recycling"></a></li>
-                            <li><a href="http://www.uff.fi/" target="_blank"><img src="<?php echo $config['paths']['base_url']; ?>/partners/uff.png" alt="UFF Auttamisen iloa"></a></li>
-                            <li><a href="http://www.kulttuurikameleontit.com/" target="_blank"><img src="<?php echo $config['paths']['base_url']; ?>/partners/kameleon.png" alt="Kulttuurikameleontit Ry"></a></li>
-                            <li><a href="http://www.nebula.fi/" target="_blank"><img src="<?php echo $config['paths']['base_url']; ?>/partners/nebula.png" alt="Nebula.fi"></a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+              <li><a href="http://www.facebook.com/siivouspaiva">Facebook</a></li>
+            </ul>
+          </div>
         </div>
-        <!-- footer ends -->
-        <script type="text/javascript">
+        <ul class="partner span4">
+          <li><a href="http://www.technoworld.fi/" target="_blank"><img src="<?php echo $config['paths']['base_url']; ?>/partners/stena.png" alt="STENA Innovative Recycling"></a></li>
+          <li><a href="http://www.lassila-tikanoja.fi" target="_blank"><img src="<?php echo $config['paths']['base_url']; ?>/partners/lt.gif" alt="Lassila tikanoja"></a></li>
+          <li><a href="http://www.kierratyskeskus.fi/" target="_blank"><img src="<?php echo $config['paths']['base_url']; ?>/partners/kk.png" alt="Kierrätyskeskus"></a></li>
+          <li><a href="http://www.uff.fi/" target="_blank"><img src="<?php echo $config['paths']['base_url']; ?>/partners/uff.gif" alt="UFF Auttamisen iloa"></a></li>
+        </ul>
+        <ul class="partner span4">
+          <li><a href="http://www.fida.info/" target="_blank"><img src="<?php echo $config['paths']['base_url']; ?>/partners/fida.png" alt="Fida International"></a></li>
+          <li><a href="http://www.kulttuurikameleontit.com/" target="_blank"><img src="<?php echo $config['paths']['base_url']; ?>/partners/kameleon.png" alt="Kulttuurikameleontit Ry"></a></li>
+          <li><a href="http://helsinkilocomotive.org/" target="_blank"><img src="<?php echo $config['paths']['base_url']; ?>/partners/hnml.png" alt="Helsinki New Media Locomotive"></a></li>
+          <li><a href="http://www.nebula.fi/" target="_blank"><img src="<?php echo $config['paths']['base_url']; ?>/partners/nebula.png" alt="Nebula.fi"></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- footer ends --> 
+<script type="text/javascript">
          $('h3').click(function(){
             var showId = $(this).attr('name');
             if( $(this).find('i.minus-icon').length > 0 ){
